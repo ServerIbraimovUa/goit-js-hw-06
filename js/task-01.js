b1119bd995c860c52927d59891bfa3c377c14ccb
ServerIbraimovUa <127730579+ServerIbraimovUa@users.noreply.github.com>
2023-07-03 17:51:39 +0300

    3task done

---
+++ js/task-01.js
@@ -0,0 +1,24 @@
+// const itemEl = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${itemEl.length}`);
+
+//!====================   children
+const listEl = document.querySelector("#categories");
+
+console.log(`Number of categories: ${[...listEl.children].length}`);
+
+[...listEl.children].forEach((item) => {
+  const title = item.querySelector("h2");
+  const items = item.querySelectorAll("li");
+  console.log(`Category: ${title.textContent}`);
+  console.log(`Elements: ${items.length}`);
+});
+
+//!=================== тут зробив через querySelectorAll
+
+// const itemsEl = listEl.querySelectorAll(".item");
+// [...itemsEl].forEach((el) => {
+//   const title = el.querySelector("h2");
+//   const items = el.querySelectorAll("li");
+//   console.log(`Category: ${title.textContent}`);
+//   console.log(`Elements: ${items.length}`);
+// });
